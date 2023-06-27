c55fac816bd8023d51d2773005ea93e3a2fb4fdc
Thomas Papageorgopoulos <101829574+Tom5052@users.noreply.github.com>
2023-06-27 20:23:28 +0300

    Import Project

---
+++ headerfiles.h
@@ -0,0 +1,20 @@
+#define T_EOF		0
+#define T_IF		2
+#define T_ELSE		10
+#define T_IMPORT	11
+#define T_PUBLIC	14
+#define T_STATIC	27
+#define T_CLASS 	29
+#define T_VOID		33
+#define T_PRINTLN	44
+#define MAX_ERRORS       0
+#define MAX_STR_CONST  256
+
+#ifndef FLEX_DEBUG
+#define FLEX_DEBUG      1
+#define SHOW_TOKENS     1
+#define SHOW_COMMENTS   1
+#define SHOW_NUMERIC    1
+#define SHOW_STRINGS    1
+#endif
+
